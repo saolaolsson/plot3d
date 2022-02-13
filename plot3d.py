741ebcc46c206252c60671d9029ceab448563708
Ola Olsson <ola.olsson@crunchfish.com>
2022-02-13 17:44:28 +0100

    Support other coordinate systems

--- plot3d.py
+++ plot3d.py
@@ -11,79 +11,71 @@ import numpy as np
 def make_axes(
         length = 1.0,
         major_tick_distance=0.1, major_tick_height=0.05,
-        minor_tick_distance=0.01, minor_tick_height=0.005):
+        minor_tick_distance=0.01, minor_tick_height=0.005,
+        color_positive=(0.1, 0.1, 0.1, 1), color_negative=(0.3, 0.3, 0.3, 1)):
     def _add_x_tick(linesegs, axis_length, tick_distance, tick_height):
-        tick_vector = p3d.Vec3(0, 0, tick_height)
-        x = 0
-        while abs(x) <= abs(axis_length):
-            point = p3d.Vec3(0, 0, 0)
-            point[0] = x
+        n_ticks = int(axis_length / tick_distance)
+        for i in range(n_ticks):
+            point = p3d.Vec3.forward() * tick_distance * i
             linesegs.move_to(point)
-            linesegs.draw_to(point + tick_vector)
-            x += tick_distance
+            linesegs.draw_to(point + p3d.Vec3.up() * tick_height)
 
-    def _make_x_axis(
-        color, length,
-        major_tick_distance, major_tick_height,
-        minor_tick_distance, minor_tick_height):
+    def _make_axis(
+            color, length,
+            major_tick_distance, major_tick_height,
+            minor_tick_distance, minor_tick_height):
         ls = p3d.LineSegs()
         ls.set_color(color)
-        ls.move_to(0, 0, 0)
-        axis_end = p3d.Point3(length, 0, 0)
-        ls.draw_to(axis_end)
+        ls.move_to(p3d.Vec3())
+        ls.draw_to(p3d.Vec3.forward() * length)
         _add_x_tick(ls, length, major_tick_distance, major_tick_height)
         _add_x_tick(ls, length, minor_tick_distance, minor_tick_height)
         return ls.create()
 
     axis = p3d.GeomNode('axis')
-    axis.add_geoms_from(_make_x_axis(
-        (0.1, 0.1, 0.1, 1), length,
+    axis.add_geoms_from(_make_axis(
+        color_positive, length,
         major_tick_distance, major_tick_height,
         minor_tick_distance, minor_tick_height))
-    axis.add_geoms_from(_make_x_axis(
-        (0.3, 0.3, 0.3, 1), -length,
+    axis.add_geoms_from(_make_axis(
+        color_negative, -length,
         -major_tick_distance, major_tick_height,
         -minor_tick_distance, minor_tick_height))
     axis = p3d.NodePath(axis)
 
     axes = p3d.NodePath('axes')
-
-    axis_x = axes.attach_new_node('axis_x')
-    axis.instance_to(axis_x)
-
-    axis_y = axes.attach_new_node('axis_y')
-    axis_y.set_hpr(90, 0, 0)
-    axis.instance_to(axis_y)
-
-    axis_z = axes.attach_new_node('axis_z')
-    axis_z.set_hpr(180, 0, -90)
-    axis.instance_to(axis_z)
-
+    for axis_vector in [p3d.Vec3.unit_x(), p3d.Vec3.unit_y(), p3d.Vec3.unit_z()]:
+        a = axes.attach_new_node('axis')
+        a.look_at(axis_vector)
+        axis.instance_to(a)
     return axes
 
 
-def make_grid(label, normal, center):
+def make_grid(label, normal, center, color=(0.35, 0.35, 0.35, 1)):
+    origin = p3d.Vec3()
+    right = p3d.Vec3.right()
+    forward = p3d.Vec3.forward()
+    up = p3d.Vec3.up()
+
     def _make_grid():
         ls = p3d.LineSegs()
+        ls.set_color(color)
 
         ls.set_thickness(1)
-        ls.set_color(0.35, 0.35, 0.35, 1)
-        for x in np.linspace(0, 1, 11, endpoint=True):
-            ls.move_to(x, 0, 0)
-            ls.draw_to(x, 0, 1)
-        for z in np.linspace(0, 1, 11, endpoint=True):
-            ls.move_to(0, 0, z)
-            ls.draw_to(1, 0, z)
+        for ri in np.linspace(0, 1, 11, endpoint=True):
+            ls.move_to(right * ri)
+            ls.draw_to(right * ri + up)
+        for ui in np.linspace(0, 1, 11, endpoint=True):
+            ls.move_to(up * ui)
+            ls.draw_to(up * ui + right)
         grid = ls.create()
 
         ls.set_thickness(2)
-        ls.set_color(0.35, 0.35, 0.35, 1)
-        ls.move_to(0, 0, 0)
-        ls.draw_to(1, 0, 0)
-        ls.draw_to(1, 0, 1)
-        ls.draw_to(0, 0, 1)
-        ls.draw_to(0, 0, 0)
-
+        ls.move_to(origin)
+        ls.draw_to(right)
+        ls.draw_to(right + up)
+        ls.draw_to(up)
+        ls.draw_to(origin)
         grid.add_geoms_from(ls.create())
         return grid
 
@@ -94,25 +86,24 @@ def make_grid(label, normal, center):
     grid = p3d.NodePath(_make_grid())
 
     g = plane.attach_new_node('')
-    g.set_pos(0, 0, 0)
     grid.instance_to(g)
 
     g = plane.attach_new_node('')
-    g.set_pos(-1, 0, 0)
+    g.set_pos(-right)
     grid.instance_to(g)
 
     g = plane.attach_new_node('')
-    g.set_pos(-1, 0, -1)
+    g.set_pos(-right - up)
     grid.instance_to(g)
 
     g = plane.attach_new_node('')
-    g.set_pos(0, 0, -1)
+    g.set_pos(-up)
     grid.instance_to(g)
 
     cn = p3d.CollisionNode('')
     plane.attach_new_node(cn)
     cn.addSolid(p3d.CollisionPolygon(
-        (-1, 0, -1), (-1, 0, 1), (1, 0, 1), (1, 0, -1)))
+        -right + up, -right - up, right - up, right + up))
 
     return plane
 
@@ -130,6 +121,13 @@ def make_cursor(size):
     return p3d.NodePath(ls.create())
 
 
+def make_axis_label(name):
+    label = p3d.TextNode('axis_label_' + name)
+    label.setText(name)
+    label.setTextColor(0.1, 0.1, 0.1, 1)
+    return p3d.NodePath(label)
+
+
 def transform_coordinate_system(point, from_space_node, to_space_node):
     return to_space_node.get_relative_point(from_space_node, point)
 
@@ -141,6 +139,18 @@ def point_ss_to_line_ws(scene, camera, lens, point_ss):
     near = transform_coordinate_system(near, camera, scene)
     far = transform_coordinate_system(far, camera, scene)
     return p3d.CollisionLine(camera.get_pos(), far - near)
+
+
+def point_ws_to_point_ss(scene, camera, lens, point_ws):
+    point_ws = transform_coordinate_system(point_ws, scene, camera)
+    point_ss = p3d.Point2()
+    r = lens.project(point_ws, point_ss)
+    if r:
+        if lens.aspect_ratio < 1:
+            point_ss.y /= lens.aspect_ratio
+        else:
+            point_ss.x *= lens.aspect_ratio
+    return point_ss
 
 
 def pick(scene, camera, lens, mouse_position):
@@ -157,8 +167,8 @@ def pick(scene, camera, lens, mouse_position):
         return None
     chq.sort_entries()
     ce = chq.entries[0]
-    if ce.get_surface_point(camera).y < 0:
-        return None
+    # if ce.get_surface_point(camera).y < 0:
+        # return None
     return ce.get_surface_point(scene)
 
 
@@ -178,6 +188,13 @@ def get_mouse_ticks(pointer, last_position):
         return p3d.Vec2(), pointer
 
 
+def position_rfu_to_xyz(right, forward, up):
+    return \
+        p3d.Vec3.right() * right + \
+        p3d.Vec3.forward() * forward + \
+        p3d.Vec3.up() * up
+
+
 def main():
     base = direct.showbase.ShowBase.ShowBase()
     base.disable_mouse()
@@ -185,12 +202,24 @@ def main():
     base.camLens.set_near(0.01)
     base.camLens.set_far(100)
 
-    make_grid('back', (0, -1, 0), (0, 1, 0)).reparent_to(base.render)
-    make_grid('ground', (0, 0, 1), (0, 0, -1)).reparent_to(base.render)
-    make_grid('left', (1, 0, 0), (-1, 0, 0)).reparent_to(base.render)
+    # p3d.loadPrcFileData('', 'coordinate-system zup_left')
+    # p3d.loadPrcFileData('', 'coordinate-system yup_right')
+
+    make_grid('back', p3d.Vec3.back(), p3d.Vec3.forward()).reparent_to(base.render)
+    make_grid('ground', p3d.Vec3.up(), p3d.Vec3.down()).reparent_to(base.render)
+    make_grid('left', p3d.Vec3.right(), p3d.Vec3.left()).reparent_to(base.render)
     make_axes(10, 1, 0.05, 0.1, 0.02).reparent_to(base.render)
     make_cursor(0.05).reparent_to(base.render)
     cursor = base.render.find('cursor')
+
+    label_scale = 0.04
+    axis_labels = [
+        make_axis_label('X'),
+        make_axis_label('Y'),
+        make_axis_label('Z')]
+    for label in axis_labels:
+        label.set_scale(label_scale)
+        label.reparent_to(base.aspect2d)
 
     mouse_wheel_delta = 0
     def _mouse_wheel_listener(delta):
@@ -203,7 +232,7 @@ def main():
         'distance_step': 0.2,
         'speed': 0.01,
         'heading_factor': 0.15,
-        'default_position': (0.5, -2, 0.5),
+        'default_position': position_rfu_to_xyz(0.5, -2, 0.5),
         'default_rotation': (15, -10, 0),
     }
     base.camera.set_pos(camera['default_position'])
@@ -215,6 +244,18 @@ def main():
     drag_ground_start_intersection = None
     orbit_start_intersection = None
     while True:
+        for label, point in zip(axis_labels, [(1, 0, 0), (0, 1, 0), (0, 0, 1)]):
+            point = point_ws_to_point_ss(base.render, base.camera, base.camLens, point)
+            if point:
+                label_offset = p3d.Vec2(
+                    -label.node().get_width() / 2,
+                    -label.node().get_height() / 2) * label_scale
+                label.set_pos(position_rfu_to_xyz(
+                    label_offset.x + point.x, 0, label_offset.y + point.y))
+                label.show()
+            else:
+                label.hide()
+
         mouse_position = get_mouse_position(base.win)
         intersection = pick(base.render, base.camera, base.camLens, mouse_position)
 
